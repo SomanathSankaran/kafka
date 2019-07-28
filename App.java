0fa3c5ecf81c217bf3e2ca8ec85f97a45f509bd6
SomanathSankaran <lalithasomanath1963@gmail.com>
2019-07-28 11:48:58 +0530

    synchronous producer

---
+++ App.java
@@ -0,0 +1,44 @@
+package com.test.kafka;
+import org.apache.kafka.clients.producer.*;  
+import java.util.*;
+import java.util.concurrent.*;
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	                                  
+        
+              String key = "Key1";
+                String value = "message test:";
+                String topicName = "SimpleProducerTopic";
+             //defining properties for kafka  
+                                
+              Properties props = new Properties();
+                props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+                props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");         
+                props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            //creating kafka producer
+                Producer<String, String> producer = new KafkaProducer<String, String>(props);                    
+            //sending messages  
+                for(int s=0;s<=10;s++){      
+                                      
+              ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName,key,value+Integer.toString(s));
+              System.out.println("sending producer");
+             
+              try {
+            	  System.out.println("sending");
+            	  System.out.println(producer.send(record).get()); 
+            	  
+                  //TimeUnit.SECONDS.sleep(1);
+                    System.out.println("SimpleProducer Completed.");
+            	  } catch (Exception e) {
+            	  e.printStackTrace();
+            	  }
+              
+              
+            }
+              producer.close();
+            
+    }
+    }
+
